cc6b59352bd329dc4eade886293e9f76abcc66a2
tr617 <tr617739@gmail.com>
2023-01-29 03:22:41 +0600

    fixed categories for live show after inset

--- src/reducers/categoryReducer.js
+++ src/reducers/categoryReducer.js
@@ -6,12 +6,41 @@ const initialState = {
     error: null
 }
 
+const buildNewCategories = (parentId, categories, category) => {
+    let myCategories = []
+    for (let cat of categories) {
+        if (cat._id == parentId) {
+            myCategories.push({
+                ...cat,
+                children: cat.children &&
+                cat.children.length > 0 ?
+                    buildNewCategories(parentId, [...cat.children, {
+                        _id: category._id,
+                        name: category.name,
+                        slug: category.slug,
+                        parentId: category.parentId,
+                        children: category.children,
+
+                    }], category) : []
+            })
+        } else {
+            myCategories.push({
+                ...cat,
+                children: cat.children &&
+                cat.children.length > 0 ?
+                    buildNewCategories(parentId, cat.children, category) : []
+            })
+        }
+    }
+    return myCategories
+}
+
 export default (state = initialState, action) => {
     if (action.type === categoryConstants.GET_ALL_CATEGORIES_SUCCESS) {
-         state = {
-             ...state,
-             categories: action.payload.categories
-         }
+        state = {
+            ...state,
+            categories: action.payload.categories
+        }
     }
     if (action.type === categoryConstants.ADD_NEW_CATEGORY_REQUEST) {
         state = {
@@ -20,15 +49,19 @@ export default (state = initialState, action) => {
         }
     }
     if (action.type === categoryConstants.ADD_NEW_CATEGORY_SUCCESS) {
+        const category = action.payload.category
+        const updatedCategories = buildNewCategories(category.parentId, state.categories, category)
+        console.log(updatedCategories)
         state = {
             ...state,
-            loading: false
+            loading: false,
+            categories: updatedCategories
         }
     }
     if (action.type === categoryConstants.ADD_NEW_CATEGORY_FAILURE) {
-          state = {
-              ...initialState
-          }
+        state = {
+            ...initialState
+        }
     }
     return state
 }
